6596f9000237d90f874da8c81eed4981684fa7fb
ROOBAN <roopavanan1009@gmail.com>
2025-07-21 14:37:39 +0530

    error remove

--- app/components/ResumeButton.tsx
+++ app/components/ResumeButton.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 const ResumeButton = () => {
-	return (
-    <Link href="/resume/rooban_bio.pdf" target="_blank" download>
+  return (
+    <a
+      href="/resume/rooban_bio.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+      download
+    >
       <button
         type="button"
-        className=" font-medium text-center px-3 py-3 flex gap-1 justify-center rounded-md transition ease-in-out delay-150 bg-[#6B7280]  hover:scale-110 hover:bg-black duration-300"
+        className="font-medium text-center px-3 py-3 flex gap-1 justify-center rounded-md transition ease-in-out delay-150 bg-[#6B7280]  hover:bg-black duration-300"
       >
         <Image
           src="/resume.svg"
@@ -18,7 +22,7 @@ const ResumeButton = () => {
         />
         <span className="lg:block hidden text-white">Download Resume</span>
       </button>
-    </Link>
+    </a>
   );
 };
 
